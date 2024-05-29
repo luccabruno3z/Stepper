746af741ff9487a679b692dbc6d16d6874924c26
luccabruno3z <luccabruno@sanluis.edu.ar>
2024-05-29 12:20:55 -0300

    Stepper.cpp

--- src/Stepper.cpp
+++ src/Stepper.cpp
@@ -257,8 +257,8 @@ void Stepper::stepMotor(int thisStep)
     switch (thisStep) {
       case 0:  // 1010
         digitalWrite(motor_pin_1, HIGH);
-        digitalWrite(motor_pin_2, LOW);
-        digitalWrite(motor_pin_3, HIGH);
+        digitalWrite(motor_pin_2, HIGH);
+        digitalWrite(motor_pin_3, LOW);
         digitalWrite(motor_pin_4, LOW);
       break;
       case 1:  // 0110
@@ -269,8 +269,8 @@ void Stepper::stepMotor(int thisStep)
       break;
       case 2:  //0101
         digitalWrite(motor_pin_1, LOW);
-        digitalWrite(motor_pin_2, HIGH);
-        digitalWrite(motor_pin_3, LOW);
+        digitalWrite(motor_pin_2, LOW);
+        digitalWrite(motor_pin_3, HIGH);
         digitalWrite(motor_pin_4, HIGH);
       break;
       case 3:  //1001
